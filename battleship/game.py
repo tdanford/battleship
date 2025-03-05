222d27d7459295793109b30cc1620ae887b21ea5
Timothy Danford <tdanford@gmail.com>
2025-03-04 21:50:58 -0500

    Refactor Game into a Game abstract class and a LocalGame concrete class.

--- battleship/game.py
+++ battleship/game.py
@@ -16,8 +16,7 @@ class GameState(Enum):
     PLAYING = "playing"
     ENDED = "ended"
 
-
-class LocalGame(MessageTarget):
+class Game(MessageTarget, ABC): 
     """A Game object is the master state of a running Battleship game.
 
     It can see the complete state of both (or, in a multiplayer future, all?) players, and
@@ -27,6 +26,102 @@ class LocalGame(MessageTarget):
     for individual players, and asynchronous interactions between players or between players and the
     game state itself.
     """
+
+    @abstractmethod
+    def send_message(self, target: str, message: Message):
+        ...
+
+    def send(self, target: str, type: str, **kwargs):
+        self.send_message(target, Message(source="game", type=type, payload={**kwargs}))
+    
+    @abstractmethod 
+    def place_ship(self, player: str, ship: Ship, start_coord: str, orientation: Orientation): 
+        ...
+
+    @abstractmethod
+    def switch_turn(self):
+        ...
+    
+    @abstractmethod 
+    def is_all_setup_complete(self): 
+        ...
+    
+    @abstractmethod 
+    def set_setup_complete(self, player: str): 
+        ...
+    
+    @abstractmethod
+    def other_player(self, player: str) -> str:
+        ...
+    
+    @abstractmethod 
+    def shoot_at_board(self, player: str, coord: str) -> Tuple[bool, Optional[PlacedShip]]:
+        ...
+    
+    @abstractmethod
+    def log(self, level, msg, *args): 
+        ...
+    
+    @abstractmethod 
+    def is_alive(self, player: str) -> bool: 
+        ...
+    
+    @abstractmethod 
+    def end_game(self, winner: str): 
+        ...
+
+    def deliver_message(self, message):
+        if message.type == "setup":
+            self.state = GameState.SETUP
+            for p in self.names:
+                self.send(p, "setup")
+        elif message.type == "ship_placed":
+            if self.state != GameState.SETUP:
+                raise ValueError(
+                    f"Illegal state {self.state} for {message.type} message"
+                )
+            ship = Ship(message.payload.get("ship"))
+            start_coord = message.payload.get("coord")
+            orientation = Orientation(message.payload.get("orientation"))
+            self.place_ship(message.source, ship, start_coord, orientation)
+        elif message.type == "setup_complete":
+            if self.state != GameState.SETUP:
+                raise ValueError(
+                    f"Illegal state {self.state} for {message.type} message"
+                )
+            self.set_setup_complete(message.source)
+            if self.is_all_setup_complete():
+                self.state = GameState.PLAYING
+                self.switch_turn()
+        elif message.type == "shot":
+            if self.state != GameState.PLAYING:
+                raise ValueError(
+                    f"Illegal state {self.state} for {message.type} message"
+                )
+
+            target_player = self.other_player(message.source)
+
+            # update internal state of the target player
+            coord = message.payload.get("coord")
+            (hit, sunk_ship) = self.shoot_at_board(target_player, coord) 
+            feedback_payload = {"coord": coord, "outcome": "hit" if hit else "miss"}
+            if sunk_ship is not None:
+                feedback_payload["sunk_ship"] = sunk_ship.ship.value
+
+            # transmit move to other player
+            self.send_message(target_player, message)
+
+            # give the first player the feedback on the shot
+            self.send(message.source, "shot_feedback", **feedback_payload)
+
+            if self.is_alive(target_player): 
+                self.switch_turn()
+            else:
+                self.end_game(message.source)
+                self.send(target_player, "lost")
+                self.send(message.source, "won")
+
+class LocalGame(Game):
 
     state: GameState
     turn: int
@@ -54,14 +149,31 @@ class LocalGame(MessageTarget):
             return self.names[0]
         else:
             raise ValueError(player)
-
-    def send(self, target: str, type: str, **kwargs):
-        self.players.get(target).deliver_message(
-            Message(source="game", type=type, payload={**kwargs})
+    
+    def end_game(self, winner: str):
+        self.state = GameState.ENDED
+        self.log(
+            logging.INFO, f"Game ended with a win for {winner}"
         )
+    
+    def is_alive(self, player: str) -> bool: 
+        return self.states[player].is_alive()
+    
+    def log(self, level, msg, *args):
+        self.logger.log(level, msg, *args)
+
+    def send_message(self, target: str, message: Message):
+        self.players.get(target).deliver_message(message)
+
+    def place_ship(self, player: str, ship: Ship, start_coord: str, orientation: Orientation): 
+        board = self.states.get(player).home_board
+        board.place_ship(PlacedShip(ship, board, start_coord, orientation))
 
     def is_all_setup_complete(self) -> bool:
         return self.setup[self.names[0]] and self.setup[self.names[1]]
+
+    def set_setup_complete(self, player: str): 
+        self.setup[player] = True
 
     def switch_turn(self):
         """Flips the `turn` variable to index the next player to play, and sends a `turn` Message to that player"""
@@ -69,69 +181,15 @@ class LocalGame(MessageTarget):
         self.print_ship_boards()
         self.send(self.names[self.turn], "turn")
 
+    def shoot_at_board(self, player: str, coord: str) -> Tuple[bool, Optional[PlacedShip]]:
+        (hit, pship) = self.states.get(player).home_board.shoot(coord)
+        self.states.get(self.other_player(player)).target_board.shoot(coord)
+        return (hit, pship)
+
     def print_ship_boards(self):
         for name, state in self.states.items():
             print(name)
             print(state.render_both_boards())
-
-    def deliver_message(self, message):
-        if message.type == "setup":
-            self.state = GameState.SETUP
-            for p in self.names:
-                self.send(p, "setup")
-        elif message.type == "ship_placed":
-            if self.state != GameState.SETUP:
-                raise ValueError(
-                    f"Illegal state {self.state} for {message.type} message"
-                )
-            ship = Ship(message.payload.get("ship"))
-            start_coord = message.payload.get("coord")
-            orientation = Orientation(message.payload.get("orientation"))
-            board = self.states.get(message.source).home_board
-            board.place_ship(PlacedShip(ship, board, start_coord, orientation))
-        elif message.type == "setup_complete":
-            if self.state != GameState.SETUP:
-                raise ValueError(
-                    f"Illegal state {self.state} for {message.type} message"
-                )
-            self.setup[message.source] = True
-            if self.is_all_setup_complete():
-                self.state = GameState.PLAYING
-                self.switch_turn()
-                for name, s in self.states.items():
-                    p = Path(f"{name}.json")
-                    p.write_text(json.dumps(s.asdict(), indent=2))
-        elif message.type == "shot":
-            if self.state != GameState.PLAYING:
-                raise ValueError(
-                    f"Illegal state {self.state} for {message.type} message"
-                )
-
-            target_player = self.other_player(message.source)
-
-            # update internal state of the target player
-            coord = message.payload.get("coord")
-            (hit, sunk_ship) = self.states.get(target_player).home_board.shoot(coord)
-            feedback_payload = {"coord": coord, "outcome": "hit" if hit else "miss"}
-            if sunk_ship is not None:
-                feedback_payload["sunk_ship"] = sunk_ship.ship.value
-
-            # transmit move to other player
-            self.players.get(target_player).deliver_message(message)
-
-            # give the first player the feedback on the shot
-            self.states.get(message.source).target_board.shoot(coord)
-            self.send(message.source, "shot_feedback", **feedback_payload)
-
-            if self.states[target_player].is_alive():
-                self.switch_turn()
-            else:
-                self.state = GameState.ENDED
-                self.logger.log(
-                    logging.INFO, f"Game ended with a win for {message.source}"
-                )
-                self.send(target_player, "lost")
-                self.send(message.source, "won")
 
     async def run(self):
         """The central loop of the game, which just checks regularly to see if the game is finished.
